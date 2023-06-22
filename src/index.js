800904da7f8e6f597434420a052b55852f929348
Eber Alejo <eber4202@hotmail.com>
2023-06-21 22:30:57 -0600

    FIX: remove all from kursor lib

--- src/index.js
+++ src/index.js
@@ -10,9 +10,3 @@ document.querySelectorAll('[data-showNav]')
 document.getElementById('toggleDarkMode').addEventListener('click', toggleDarkMode)
 
 document.addEventListener('scroll', toggleNavShadow)
-
-// eslint-disable-next-line no-unused-vars, new-cap, no-undef
-const kursorx = new kursor({
-  type: 2,
-  color: '#5E81AC'
-})
