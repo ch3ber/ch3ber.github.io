12c6b7837ccd191f997f258667596a57b61be427
Eber Alejo <eber4202@hotmail.com>
2025-07-22 09:38:37 -0600

    fix(config): remove slug from config

--- src/content/config.ts
+++ src/content/config.ts
@@ -4,7 +4,6 @@ export const collections = {
   projects: defineCollection({
     schema: z.object({
       title: z.string(),
-      slug: z.string(),
       image: z.string(), // ruta relativa a /public
       repo: z.string().url(),
       demo: z.string().url().optional(),
